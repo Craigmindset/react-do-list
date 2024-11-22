c0168adfd700ca92f72864c43ca52eee69db6c71
craigmindset <irediacraig@gmail.com>
2024-11-22 16:35:27 +0100

    react

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import Task from './components/Task';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <SafeAreaView style={styles.container}>
+      <StatusBar translucent={true} backgroundColor="transparent" />
+       
+       <View style={styles.container}>
+     {/*task start her */}
+     <View style={styles.taskWrapper}>
+      <Text style={styles.sectionTitle}>Task to Do</Text>
+     </View>
+       
+       <View style={styles.items}> 
+         {/*This is where all the styles*/} 
+         <TouchableOpacity> <Task text={'Task 1'}/> </TouchableOpacity>
+         
+         <Task text={'Task 2'}/>
+         <Task text={'Task 3'}/>
+         <Task text={'Task 4'}/>
+         <Task text={'Task 5'}/>
+        
+        
+       </View>
+
     </View>
+
+    </SafeAreaView>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ADD8E6',
+    
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginBottom: 20,
+  },
+  items: {},
+  
+
 });
